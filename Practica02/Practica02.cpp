67da1c5b2e1b0fc65e06d3b0235a241795cfe2a2
hazel-hernandez022 <hazel.perez79@unach.mx>
2023-10-01 20:46:37 -0500

    Agregar archivos de proyecto.

---
+++ Practica02/Practica02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+const int NUM = 8;
+int main()
+{
+	int nums[NUM];
+	int total = 0;
+	for (int i = 0; i < NUM; i++)
+	{
+		cout << "por favor, introduzca un numero";
+		cin >> nums[i];
+		total += nums[i];
+
+	}
+	cout << "el total de numeros es " << total << endl;
+	return 0;
+}
